8d79aa1e329a023f73bfbadb4a44d4001fc8311c
Vilhelmternevik <Vilhelm.ternevik@elev.ga.lbs.se>
2023-02-22 16:04:42 +0100

    added score counter

---
+++ Assets/Scripts/UI/ScoreCounter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ScoreCounter : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public float count = 0;
+    private void Start()
+    {
+        text.text = count.ToString();
+    }
+
+    public void IncreaseScore(float increase)
+    {
+        count += increase;
+        text.text = count.ToString();
+    }
+}
