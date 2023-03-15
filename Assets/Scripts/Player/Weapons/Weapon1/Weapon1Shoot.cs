c8cdb09aa791f19f0d9df1c858db1ff29d421b3a
Bloodhound-06 <ollipolli06@gmail.com>
2023-03-15 13:48:45 +0100

    Weapon Function update

--- Assets/Scripts/Player/Weapons/Weapon1/Weapon1Shoot.cs
+++ Assets/Scripts/Player/Weapons/Weapon1/Weapon1Shoot.cs
@@ -1,6 +1,7 @@
 //using System.Collections;
 //using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Weapon1Shoot : MonoBehaviour
 {
@@ -8,13 +9,13 @@ public class Weapon1Shoot : MonoBehaviour
     [Header("Private")]
     private Vector3 targetPos; //the position of the mouse
     private Vector3 firePoint; //the origin of the raycast
-    private Vector3 distance;
 
     [Header("Floats")]
     public float fireDelay; //the delay between when you can fire
     public float maxBullet; //the amount of bullets in a full magazine
     public float currentBullet; //the amount of bullet remaining in the magazine
     public float reloadTime; //the time it takes to reload
+    
 
     [Header("Bools")]
     public bool CanFire; //can the player fire?
@@ -22,9 +23,12 @@ public class Weapon1Shoot : MonoBehaviour
     [Header("References")]
     public GameObject aimPoint; //the position of where to fire
     public GameObject hitObject; //the game object that gets hit by the raycast
+    public GameObject invisiLaser; //the invisible laser
+    public GameObject GunLaser; //the laser the gun fires
     public GameObject pm; //the pause menu
     public Transform firePosition; //the position of the gun
     public PausMenu pmS; //the pause menu script
+    public TextMeshProUGUI ammoCounter; //the game object of the ammo counter text
 
 
     [Header("Keybinds")]
@@ -32,32 +36,43 @@ public class Weapon1Shoot : MonoBehaviour
     public KeyCode shootKey; //the keybind to fire
     private void Start()
     {
+        AmmoCounter();
+
+        GunLaser = GameObject.Find("Laser"); //gets the laser
         aimPoint = GameObject.Find("cursor"); //sets the aimpoint
         CanFire = true; //set can fire to true
         currentBullet = maxBullet; //sets current bullets to max
+        
+        pm = GameObject.Find("PauseMenu"); //gets the pause menu
+        pmS = pm.GetComponent<PausMenu>(); //gets the pause menu script
 
-        pm = GameObject.Find("PauseMenu");
-        pmS = pm.GetComponent<PausMenu>(); //gets the 
+        GunLaser.SetActive(false);
     }
 
     private void Update()
     {
-        distance = firePoint - targetPos;
+        
         firePoint = new Vector3(firePosition.position.x, firePosition.position.y); //sets the fire point to the firepoint position
         targetPos = aimPoint.transform.position - transform.position; //sets the targeting posiotion to the mouse
 
+
         Debug.DrawRay(firePoint, targetPos, Color.red); //draws a red line in the editor where the raycast is
+
 
         if (Input.GetKeyDown(shootKey) && CanFire == true) //is the shoot key pressed down
         {
             CanFire = false; //sets can fire to false
             Shoot(); //calls shoot
+            Laser();
+            LaserOn();
             Invoke(nameof(ResetFire), fireDelay); //calls reset fire on a delay
+            
         }
 
 
         if (Input.GetKey(reloadKey)) //if the reload key is pressed
         {
+            currentBullet = 0;
             CanFire = false; //sets can fire to false
             Invoke(nameof(Reload), reloadTime); //calls reload with a delay
         }
@@ -68,7 +83,8 @@ public class Weapon1Shoot : MonoBehaviour
     private void Shoot()
     {
         currentBullet--; //removes one bullet from current bullets
-        RaycastHit2D hit = Physics2D.Raycast(firePoint, targetPos); //creates a raycast from player to aimpoint
+        AmmoCounter();
+        RaycastHit2D hit = Physics2D.Raycast(firePoint, targetPos, 1000); //creates a raycast from player to aimpoint
 
         if (hit.collider != null && hit.collider.CompareTag("Enemy")) //if the raycast hits and if the hit target is a enemy
         {
@@ -78,11 +94,17 @@ public class Weapon1Shoot : MonoBehaviour
         }
     }
 
+    private void LaserOn()
+    {
+        GunLaser.SetActive(true);
+    }
 
 
     private void ResetFire()
     {
-        if (currentBullet <= 0) //if current bullets är mindre än 1
+        LaserOff();
+
+        if (currentBullet <= 0) //if current bullets are less than 1
         {
             CanFire = false; //sets can fire to 
         }
@@ -90,6 +112,16 @@ public class Weapon1Shoot : MonoBehaviour
         {
             CanFire = true; //sets can fire to true
         }
+    }
+
+    private void LaserOff()
+    {
+        GunLaser.SetActive(false);
+    }
+
+    private void Laser()
+    {
+            GunLaser.transform.SetPositionAndRotation(invisiLaser.transform.position, invisiLaser.transform.rotation);
     }
 
     private void Reload()
@@ -107,6 +139,30 @@ public class Weapon1Shoot : MonoBehaviour
         else if (!pmS.paused)
         {
             CanFire = true;
+        }
+    }
+
+    private void AmmoCounter()
+    {
+        if (currentBullet == 0)
+        {
+            ammoCounter.color = Color.red;
+            ammoCounter.text = ("Empty");
+        }
+        else if(currentBullet < 6 && currentBullet > 0)
+        {
+            ammoCounter.color = Color.red;
+            ammoCounter.text = currentBullet.ToString();
+        }
+        else if(currentBullet < 11 && currentBullet > 5)
+        {
+            ammoCounter.color = Color.yellow;
+            ammoCounter.text = currentBullet.ToString();
+        }
+        else
+        {
+            ammoCounter.color = Color.white;
+            ammoCounter.text = currentBullet.ToString();
         }
     }
 }
