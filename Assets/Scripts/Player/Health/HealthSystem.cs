f6e9ac40518f60d0eb2fccfed6b3726d9c21e0c5
Bloodhound-06 <ollipolli06@gmail.com>
2023-04-06 10:45:30 +0200

    Game Over and prefabs fix

--- Assets/Scripts/Player/Health/HealthSystem.cs
+++ Assets/Scripts/Player/Health/HealthSystem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class HealthSystem : MonoBehaviour
 {
@@ -35,7 +36,7 @@ public class HealthSystem : MonoBehaviour
 
         if(currentHealth < 0) //if hp = 0
         {
-            //death //die
+            SceneManager.LoadScene(2);
         }
     }
 }
--- Assets/Scripts/Scenes/SceneManagerScript.cs
+++ Assets/Scripts/Scenes/SceneManagerScript.cs
@@ -9,29 +9,33 @@ public class SceneManagerScript : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
-    
+
     public void LoadScene1()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 
     public void LoadScene2()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(2);
     }
 
     public void LoadScene3()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(3);
     }
 
     public void LoadScene4()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(4);
     }
 
     public void LoadScene5()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(5);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
